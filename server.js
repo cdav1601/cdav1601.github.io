ae890e442741556f49ef396eb1e0ac3cd8ddfb9b
cdav1601 <45018911+cdav1601@users.noreply.github.com>
2020-11-04 03:35:14 -0500

    L7

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -20,13 +21,15 @@ app.use((req, res, next) => {
 });
 
 app.route('/api')
-  .get((req, res) => {
+  .get(async(req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
+    console.log('fetch request data', data);
   })
-  .post((req, res) => {
+  .post(async(req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = data.json();
+    res.json(json);
   });
 
 app.listen(port, () => {
